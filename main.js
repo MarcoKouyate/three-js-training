09127aabd71ae979a6aa3190b0dfa38b7373d8c5
Marco Kouyaté <marco.kouyate@yahoo.fr>
2022-02-17 13:56:56 +0100

    ThreeJS empty scene setup

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+import { ceilPowerOfTwo } from 'three/src/math/mathutils';
+
+const scene = new THREE.Scene();
+
+const pov = 75;
+const aspectRatio = window.innerWidth/window.innerHeight;
+const near = 0.1;
+const far = 1000;
+
+
+const camera = new THREE.PerspectiveCamera(pov, aspectRatio, near, far)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
